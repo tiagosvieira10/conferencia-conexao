6e05a197535e9dfa788fa52dd4ba94d8706a5983
Tiago Vieira <tiagosvieira10@gmail.com>
2023-11-09 17:27:48 -0300

    footer add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import { HomeContainer, HomeMenu } from '@/styles/home'
-import bg from '../assets/site-conferencia (2).png'
 import logoConf24 from '../assets/next (2).svg'
 import logTheMission from '../assets/Conferencia 24.svg' 
 import Image from "next/image"
+import sublinhado from '../assets/sublinhado.svg'
 
 export default function Home() {
   return(
@@ -14,6 +14,10 @@ export default function Home() {
         <button className='line-up'>Line-up do evento</button>
         <button className='ingresso'><strong>Garantir ingresso</strong><br />( 1º lote)</button>
       </HomeMenu>
+
+      <footer>
+        <Image src={sublinhado} alt='' width={350} height={350}/>
+      </footer>
     </HomeContainer>
    
   )
