72f9e4836c1c442eb3fd2cb114f1dd1a8d818eee
Tiago Vieira <tiagosvieira10@gmail.com>
2023-11-09 14:27:01 -0300

    img bg add and img logo import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import { HomeContainer } from '@/styles/home'
-import bg from '../assets/site-conferencia.png'
+import bg from '../assets/site-conferencia (2).png'
+import logoConf24 from '../assets/next (1).svg'
+import logTheMission from '../assets/Conferencia 24.svg' 
 import Image from "next/image"
 
 export default function Home() {
   return(
     <HomeContainer>
       <Image src={bg} alt=""/>
-      <h1>Hello world</h1>
+      
     </HomeContainer>
    
   )
--- src/styles/home.ts
+++ src/styles/home.ts
@@ -1,9 +1,19 @@
 import { styled } from ".";
 
 export const HomeContainer = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
 
   img: {
-    maxWidth: '100%',
-    maxHeight: '100vh',
+    height: '100vh',
+    width: '100%', 
+    opacity: 0.4,
+  },
+
+  '@media screen and (max-width: 768px)': {
+    img: {
+      height: '100vh',
+      width: '100%', 
+    },
   }
 })
